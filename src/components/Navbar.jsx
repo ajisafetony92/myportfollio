531063ca0d72103d4b7f656c59d0b23b21b89de0
Anthony Olayinka <anthonyolaola@gmail.com>
2026-05-04 11:08:02 +0100

    other stuffs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
 
   const checkAuth = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/auth/me", {
+      const response = await fetch("https://myportfollio-zo2p.onrender.com//api/auth/me", {
         credentials: "include"
       });
 
@@ -35,7 +35,7 @@ function Navbar() {
 
   const handleLogout = async () => {
     try {
-      await fetch("http://localhost:5000/api/auth/logout", {
+      await fetch("https://myportfollio-zo2p.onrender.com//api/auth/logout", {
         method: "POST",
         credentials: "include"
       });
